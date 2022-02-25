4f33fcef253a92602157542de7a6bfafa2432597
narekkirakosyan <narek.kirakosyan@picsart.com>
2022-02-25 18:19:02 +0400

    Initial Commit

---
+++ Magic Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic Reader
+//
+//  Created by Narek Kirakosyan on 25.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
