e2ba8d9ce0d355fcc12ce388b22a4c6e31c67b11
taronpicsart <taron.vekilyan@picsart.com>
2022-02-26 04:38:35 +0400

    Initial commit

--- Magic Reader/ViewController.swift
+++ Magic Reader/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tap))
+        mainImage.isUserInteractionEnabled = true
+        mainImage.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc private func tap() {
+        guard let url = URL(string: "myImageTracker://a") else { return }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .darkContent
+    }
 }
 
